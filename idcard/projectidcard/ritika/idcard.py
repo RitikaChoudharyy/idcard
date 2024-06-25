bcee2db10672108593745e40fc7695c96f4e122e
RitikaChoudharyy <ritika.21jics053@jietjodhpur.ac.in>
2024-06-25 10:16:15 +0530

    idcard.py

--- idcard/projectidcard/ritika/idcard.py
+++ idcard/projectidcard/ritika/idcard.py
@@ -180,7 +180,7 @@ def main():
     st.title("Automatic ID Card Generation")
     
     template_path = "C:\\Users\\Shree\\Desktop\\idcard\\projectidcard\\ritika\\ST.png"
-    image_folder = "C:\\Users\\Shree\\Desktop\\idcard\\projectidcard\\ritika"
+    image_folder = "C:\Users\Shree\Desktop\idcard\projectidcard\ritika"
     qr_folder = "C:\\Users\\Shree\\Desktop\\idcard\\projectidcard\\ritika\\ST_output_qr_codes"
     output_pdf_path = "C:\\Users\\Shree\\Desktop\\generated_id_cards.pdf"
 
