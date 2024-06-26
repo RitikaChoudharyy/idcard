c31032bbbf04a9ed7837dab5d1d113f8c0bf2007
RitikaChoudharyy <ritika.21jics053@jietjodhpur.ac.in>
2024-06-26 11:08:39 +0530

    idcard.py

--- idcard/projectidcard/ritika/idcard.py
+++ idcard/projectidcard/ritika/idcard.py
@@ -181,9 +181,7 @@ def display_pdf(pdf_path):
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
         pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
         st.markdown(pdf_display, unsafe_allow_html=True)
-
-
-    def main():
+def main():
     # Streamlit setup
      st.title("Automatic ID Card Generation")
 
